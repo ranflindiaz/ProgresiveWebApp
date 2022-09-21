ff42790e0d99eb409654be7f8e46bd5f7704b8c4
Ranflin Diaz <54969258+ranflindiaz@users.noreply.github.com>
2022-09-20 23:37:05 -0400

    Add project files.

---
+++ ProgresiveWebApp/wwwroot/js/menu.js
@@ -0,0 +1,14 @@
+﻿const burgerMenu = document.getElementById("burger");
+const navbarMenu = document.getElementById("menu");
+
+//Show and Hide menu
+burgerMenu.addEventListener("click", () => {
+    burgerMenu.classList.toggle("is-active");
+    navbarMenu.classList.toggle("is-active");
+
+    if (navbarMenu.classList.contains("is-active")) {
+        navbarMenu.style.maxHeight = navbarMenu.scrollHeight + "px";
+    } else {
+        navbarMenu.removeAttribute("style");
+    }
+});
