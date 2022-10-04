9347442de86e0079cb6fb1874f9fe158bd4b0d29
Ranflin Diaz <54969258+ranflindiaz@users.noreply.github.com>
2022-10-03 22:19:32 -0400

    Saving files on cache

--- ProgresiveWebApp/wwwroot/service-worker.js
+++ ProgresiveWebApp/wwwroot/service-worker.js
@@ -1,7 +1,43 @@
-﻿self.addEventListener("install", event => {
+﻿
+//variable to read array
+const staticCacheName = "staticCache";
+
+//array with static files to load page offline
+const staticFiles = ["/css/menu.css",
+    "/ProgresiveWebApp.styles.css",
+    "/lib/jquery/dist/jquery.min.js",
+    "/lib/bootstrap/dist/js/bootstrap.bundle.min.js",
+    "/js/menu.js", "/"];
+
+self.addEventListener("install", event => {
     console.log("Worker Installed");
+
+    //saving cache files
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            return cache.addAll(staticFiles);
+        })
+    );
 });
 
 self.addEventListener("activate", event => {
     console.log("Worker Activeated");
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+
+    //loading cache files
+    const response = caches.match(event.request).then(res => {
+        if (res) return res;
+        else {
+            return fetch(event.request).then(response => {
+                return response;
+            })
+        }
+    }).catch(err => {
+        return null;
+    });
+
+    event.respondWith(response);
 });
